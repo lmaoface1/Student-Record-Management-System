bbdaee85a4b00bfc7140da434ae658d914fca27d
Jan Mark Torres <torres.janmark21@gmail.com>
2026-03-01 04:24:05 +0800

    added backend structure (backbone)

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "", 
+  database: "student_db",
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
+  }
+  console.log("Connected to MySQL Database!");
+  connection.release();
+});
+
+module.exports = pool.promise();
